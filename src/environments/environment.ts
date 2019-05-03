2a1d8ef026c94241ddb3730b60423673cd1d83d9
runthegit <36576757+runthegit@users.noreply.github.com>
2019-05-03 12:44:51 -0500

    FIrst Commit: google firebaseAuth w/ mat. toolbar

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDoarEAY2ZQKrVB-KHK1Y0itHnv6fKnc1g",
+    authDomain: "angularfirebaseproj-ec38d.firebaseapp.com",
+    databaseURL: "https://angularfirebaseproj-ec38d.firebaseio.com",
+    projectId: "angularfirebaseproj-ec38d",
+    storageBucket: "angularfirebaseproj-ec38d.appspot.com",
+    messagingSenderId: "407223919330"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
